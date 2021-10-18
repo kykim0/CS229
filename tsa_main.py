538db8a806554e797147f0d6e70b1689acb6bd61
kykim <kykim144@gmail.com>
2021-10-17 22:10:04 -0700

    Support training more stats models.

--- tsa_main.py
+++ tsa_main.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import data
+import flags as cflags
 import metrics
 import stat_models
 
@@ -26,7 +27,8 @@ flags.DEFINE_string('filepath', 'data/jena_climate_2009_2016.csv',
                     'Path to a csv data file.')
 
 flags.DEFINE_enum('model', None,
-                  ['ar', 'ma', 'arma', 'arima', 'sarima', 'rnn'],
+                  ['ar', 'ma', 'arma', 'arima', 'sarima',
+                   'ses', 'holt', 'holtwinters'],
                   'The type of time-series model to use.')
 
 flags.DEFINE_string('start_date', None, 'Start date for the data.')
@@ -44,6 +46,34 @@ flags.DEFINE_bool('plot_diagnostics', False,
                   'True to plot model diagnostics.')
 
 
+def fit_and_forecast(model, train_df, test_df, tcolumn, forecast_steps):
+  """Fits a stats model and returns fitted values and forecasts."""
+  y_true = train_df[tcolumn].to_numpy()
+  train_len, test_len = len(train_df.index), len(test_df.index)
+
+  model_res, fitted_values, predictions = None, None, None
+  if model == 'ar':
+    lags = FLAGS.lags
+    model_res = stat_models.autoreg_model(y_true, lags=lags, select_order=True)
+  elif model == 'ses':
+    model_res = stat_models.simple_exp_smoothing(y_true)
+  elif model == 'holt':
+    exp_trend = FLAGS.exp_trend
+    damped_trend = FLAGS.damped_trend
+    model_res = stat_models.holt_exp_smoothing(y_true, exp_trend=exp_trend,
+                                               damped_trend=damped_trend)
+  elif model == 'holtwinters':
+    model_res = stat_models.holt_winters_exp_smoothing()
+
+  fitted_values = model_res.fittedvalues
+  predictions = model_res.forecast(forecast_steps)
+  print(y_true[:30])
+  print(fitted_values[:30])
+  print(model_res.params)
+  print(model_res.summary())
+  return model_res, fitted_values, predictions
+  
+
 def plot_fit_pred(train_df, test_df, tcolumn, fitted_values, predictions):
   """Creates a plot of fitted values and predictions
 
@@ -51,13 +81,13 @@ def plot_fit_pred(train_df, test_df, tcolumn, fitted_values, predictions):
     train_df: (DataFrame) training data.
     test_df: (DataFrame) test data.
     tcolumn: (str) name of the depedent variable column.
-    fitted_values: (np.array) in-sample fitted values.
-    predictions: (np.array) out-of-sample predictions.
+    fitted_values: (np.ndarray) in-sample fitted values.
+    predictions: (np.ndarray) out-of-sample predictions.
   """
   # Plot the training and test data.
   fig = plt.figure(figsize=(10, 6))
   ax = fig.subplots()
-  ax.set_title('Temperature in Celsius')
+  ax.set_title(tcolumn)
   ax.plot(train_df.index, train_df[tcolumn])
   ax.plot(test_df.index, test_df[tcolumn])
 
@@ -65,7 +95,7 @@ def plot_fit_pred(train_df, test_df, tcolumn, fitted_values, predictions):
   # Note that the length of `fitted_values` may not match the size of the
   # training dataset if, for instance, a long lag was used for an AR model.
   ax.plot(train_df.index[-len(fitted_values):], fitted_values, '--')
-  ax.plot(test_df.index, predictions)
+  ax.plot(test_df.index, predictions, '--')
   ax.legend(labels=['Train', 'Test', 'Fit', 'Pred'])
   return fig
 
@@ -82,24 +112,18 @@ def main(argv=()):
   tcolumn = FLAGS.dependent_col_name
   forecast_steps = FLAGS.forecast_steps
   train_df, test_df = df.iloc[:-forecast_steps], df.iloc[-forecast_steps:]
-  y_true = train_df[tcolumn].to_numpy()
-  model_res = stat_models.autoreg_model(y_true, lags=30, select_order=True)
-  print(model_res.params)
-  print(model_res.summary())
+  model_res, fitted_values, predictions = fit_and_forecast(
+    FLAGS.model, train_df, test_df, tcolumn, forecast_steps)
 
-  train_len, test_len = len(train_df.index), len(test_df.index)
-  fitted_values = model_res.fittedvalues
-  predictions = model_res.predict(train_len, train_len + test_len - 1)
+  print(test_df[tcolumn].to_numpy()[-30:])
+  print(predictions[-30:])
+  mae = metrics.mean_absolute_error(test_df[tcolumn].to_numpy(), predictions)
+  print(f'Mean absolute error: {mae}')
+
   plot_fit_pred(train_df, test_df, tcolumn, fitted_values, predictions)
-
-  print(test_df[tcolumn].to_numpy()[:30])
-  print(predictions[:30])
-  print(metrics.mean_absolute_percent_error(test_df[tcolumn].to_numpy(),
-                                            predictions))
-  
   if FLAGS.plot_diagnostics:
     fig = plt.figure(figsize=(16, 9))
-    model_res.plot_diagnostics(fig=fig, lags=30)
+    model_res.plot_diagnostics(fig=fig)
   plt.show()
 
 
