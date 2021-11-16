c752adcfa99b7e632e07f6d0f379a5f9baca8bf4
kykim <kykim144@gmail.com>
2021-11-15 16:41:33 -0800

    Add scratch.py for various testings, plottings, etc.

---
+++ scratch.py
@@ -0,0 +1,31 @@
+import os
+
+from absl import app
+from absl import flags
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import data
+import flags as cflags  # pylint: disable=unused-import
+
+sns.set_style('darkgrid')
+
+FLAGS = flags.FLAGS
+
+
+def main(argv=()):
+  del argv
+
+  filepath = os.path.abspath(FLAGS.filepath)
+  start_date, end_date = FLAGS.start_date, FLAGS.end_date
+  df = data.read_data_df(FLAGS.dataset_name, filepath,
+                         start_date, end_date)
+
+  corr = df.corr(method='spearman')
+  sns.heatmap(corr.round(2), annot=True)
+  plt.show()
+
+
+if __name__ == '__main__':
+  app.run(main)
