86739819ddbbc00eb031276f49abbbfb24ac4cb3
kykim <kykim144@gmail.com>
2021-11-17 18:02:14 -0800

    Add SARIMAX model.

--- stat_models.py
+++ stat_models.py
@@ -1,6 +1,6 @@
 """Classical statistical time-series models."""
 
-from statsmodels.tsa import api, ar_model
+from statsmodels.tsa import api, ar_model, statespace
 
 
 def simple_exp_smoothing(endog):
@@ -66,6 +66,7 @@ def autoreg_model(endog, lags, select_order=False, trend='c', exog=None,
     endog: (np.ndarray) 1d dependent values.
     lags: (int,list[int]) Number of lags to include in the AR model.
     select_order: (bool) if True use `ar_model_order` to select order.
+    trend: (str) can be 'n' (none), 'c' (constant), 't' (linear), 'ct' (both)
     exog: (np.ndarray) covariates (exogenous) variables.
     period: (int) period of data, applicable for seasonal data.
   """
@@ -78,5 +79,22 @@ def autoreg_model(endog, lags, select_order=False, trend='c', exog=None,
   else:
     model = ar_model.AutoReg(endog, lags, trend=trend, seasonal=seasonal,
                              exog=exog, period=period)
+  model_res = model.fit()
+  return model_res
+
+
+def sarimax_model(endog, exog=None, order=(1, 0, 0), sorder=(0, 0, 0, 0),
+                  trend='c'):
+  """Fits an ARIMA model with optional seasonality and covariates.
+
+  Args:
+    endog: (np.ndarray) 1d dependent values.
+    exog: (np.ndarray) optional explanatory variables to model.
+    order: (iterable) p, d, q of the non-seasonal component.
+    sorder: (iterable) P, D, Q, s of the seasonal component.
+    trend: (str) can be 'n' (none), 'c' (constant), 't' (linear), 'ct' (both)
+  """
+  model = statespace.sarimax.SARIMAX(endog, exog=exog, order=order,
+                                     seasonal_order=sorder, trend=trend)
   model_res = model.fit()
   return model_res
