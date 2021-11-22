252bed8a52297aeb3bfd57ed7131c48a8d455d72
kykim <kykim144@gmail.com>
2021-11-21 16:21:14 -0800

    Make the fitted_values and predictions args optional.

--- plot.py
+++ plot.py
@@ -7,8 +7,9 @@ import seaborn as sns
 sns.set_style('darkgrid')
 
 
-def plot_fit_pred(train_df, test_df, endog_col, fitted_values, predictions):
-  """Creates a plot of fitted values and predictions
+def plot_fit_pred(train_df, test_df, endog_col,
+                  fitted_values=None, predictions=None):
+  """Creates a plot of fitted values and predictions.
 
   Args:
     train_df: (DataFrame) training data.
@@ -23,11 +24,16 @@ def plot_fit_pred(train_df, test_df, endog_col, fitted_values, predictions):
   ax.set_title(endog_col)
   ax.plot(train_df.index, train_df[endog_col], '.-')
   ax.plot(test_df.index, test_df[endog_col], '.-')
+  labels = ['Train', 'Test']
 
   # Plot the fitted values and predictions.
   # Note that the length of `fitted_values` may not match the size of the
   # training dataset if, for instance, a long lag was used for an AR model.
-  ax.plot(train_df.index[-len(fitted_values):], fitted_values, '.--')
-  ax.plot(test_df.index, predictions, '.--')
-  ax.legend(labels=['Train', 'Test', 'Fit', 'Pred'])
+  if fitted_values is not None:
+    ax.plot(train_df.index[-len(fitted_values):], fitted_values, '.--')
+    labels.append('Fit')
+  if predictions is not None:
+    ax.plot(test_df.index, predictions, '.--')
+    labels.append('Pred')
+  ax.legend(labels=labels)
   return fig
